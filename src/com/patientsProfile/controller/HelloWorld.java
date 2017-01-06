2e6c90cc3a770dfddc9bd8d0e06a8c122a158353
saquibw <saquib.waheed@yahoo.com>
2017-01-07 01:08:38 +0600

    Project launched successfully

---
+++ src/com/patientsProfile/controller/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.patientsProfile.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(value="/")
+public class HelloWorld {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String helloWorld(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model){
+		System.out.println("Entered this method");
+		model.addAttribute("name", name);
+		return "welcome";
+	}
+
+}
