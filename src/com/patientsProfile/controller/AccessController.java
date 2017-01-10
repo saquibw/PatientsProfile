0c6d77f47c3b4524a5f8dcb2a33ca038f30b3914
saquibw <saquib.waheed@yahoo.com>
2017-01-11 04:20:58 +0600

    login logout logic applied

--- src/com/patientsProfile/controller/AccessController.java
+++ src/com/patientsProfile/controller/AccessController.java
@@ -1,29 +1,32 @@
 package com.patientsProfile.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.patientsProfile.model.User;
-import com.patientsProfile.service.UserService;
-
 @Controller
 public class AccessController {
-	
-	@Autowired
-	private UserService userService;
-	
+
 	@RequestMapping(value="/")
 	public String redirect(){
 		return "redirect:/login";
 	}
-	
+
 	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public String login(){
-		User user = userService.getByUsername("shafiqur");
-		System.out.println(user.getUserName());
 		return "login";
+	}
+
+	@RequestMapping(value = "/login/failure")
+	public String loginFailure() {
+		String message = "Login Failure!";
+		return "redirect:/login?message="+message;
+	}
+
+	@RequestMapping(value = "/logout/success")
+	public String logoutSuccess() {
+		String message = "Logout Success!";
+		return "redirect:/login?message="+message;
 	}
 
 }
